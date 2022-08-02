7e047013b4079801756d103e685a77ccdbb53a1e
Ryan Landgren <Ryan Landgren@LAPTOP-QE23SUP1>
2022-08-01 23:48:59 -0500

    Add project files.

---
+++ Battery Percentage/Program.cs
@@ -0,0 +1,42 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+
+        public Program()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update1;
+        }
+      
+        public void Main(string argument, UpdateType updateSource)
+        {
+
+        }
+    }
+}
+/* stored, output. Stored power represented as |█| (mulitples of 10%)
+ *                              Stored Power: |█|█|█|█|█|█|█|█|█|█|
+ *                              Displayed on seperate montior:
+ *                              1h (now)
+ *                              30m (past 5m of  usage)
+ */
