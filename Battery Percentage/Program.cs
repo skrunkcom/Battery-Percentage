e31891e168153338829eea415537d7c338b0d5a0
Ryan Landgren <Ryan Landgren@LAPTOP-QE23SUP1>
2022-08-03 00:23:50 -0500

    Fuck You

--- Battery Percentage/Program.cs
+++ Battery Percentage/Program.cs
@@ -23,62 +23,62 @@ namespace IngameScript
     partial class Program : MyGridProgram
     {
 
-        public Program()
-        {
-            Runtime.UpdateFrequency = UpdateFrequency.Update1;
-        }
+            public Program()
+            {
+                Runtime.UpdateFrequency = UpdateFrequency.Update1;
+            }
       
-        public void Main(string argument, UpdateType updateSource)
-        {
-            var batts = new List<IMyBatteryBlock>();
-            GridTerminalSystem.GetBlocksOfType<IMyBatteryBlock>(batts);
-            var mybatts = new List<IMyBatteryBlock>();
+            public void Main(string argument, UpdateType updateSource)
+            {
+                var batts = new List<IMyBatteryBlock>();
+                GridTerminalSystem.GetBlocksOfType<IMyBatteryBlock>(batts);
+                var mybatts = new List<IMyBatteryBlock>();
             
-            // Filter out only batteries on our grid
-            foreach (IMyBatteryBlock batt in batts)
-            {
-                long bg = batt.CubeGrid.EntityId; // Battery on Grid of programmable block
-                long cg = Me.CubeGrid.EntityId;   // Grid on the programmable block
-                if (cg == bg)
+                // Filter out only batteries on our grid
+                foreach (IMyBatteryBlock batt in batts)
                 {
-                    mybatts.Add(batt);
+                    long bg = batt.CubeGrid.EntityId; // Battery on Grid of programmable block
+                    long cg = Me.CubeGrid.EntityId;   // Grid on the programmable block
+                    if (cg == bg)
+                    {
+                        mybatts.Add(batt);
+                    }
                 }
+
+                /* Calcaulate sum of values */
+                float SumSp = 0;   //Sum of sp
+                float SumMsp = 0;  //average of msp
+                float Sumop = 0;   //average of op
+                foreach (IMyBatteryBlock mybatt in mybatts)
+                {
+                    float Sp;  //Stored Power
+                    float Msp; //Max Stored Power
+                    float Op;  // Output
+                    Sp = mybatt.CurrentStoredPower;
+                    Msp = mybatt.MaxStoredPower;
+                    Op = mybatt.CurrentOutput;
+
+                    SumSp = SumSp + Sp;
+                    SumMsp = SumMsp + Msp;
+                    Sumop = Sumop + Op;  
+                }
+                // Take averages of sums
+                float AvgSp = SumSp / mybatts.Count;   // Stored Power in MWh
+                float AvgMsp = SumMsp / mybatts.Count; // Max stored power in MWh
+                float AvgOp = Sumop / mybatts.Count;   // Output in MW
+
+
+
+                int pb = (int)(AvgSp / AvgMsp * 10); // Power in Seconds
+                Echo(pb.ToString());
+
+
+
+                int pn = (int)(AvgSp / AvgOp * 60 * 60);
+                string seconds = TimeSpan.FromSeconds(pn).ToString(@"ddd\.hh\:mm\:ss") + " remaining";
+                Echo(seconds);
+
             }
-
-            /* Calcaulate sum of values */
-            float SumSp = 0;   //Sum of sp
-            float SumMsp = 0;  //average of msp
-            float Sumop = 0;   //average of op
-            foreach (IMyBatteryBlock mybatt in mybatts)
-            {
-                float Sp;  //Stored Power
-                float Msp; //Max Stored Power
-                float Op;  // Output
-                Sp = mybatt.CurrentStoredPower;
-                Msp = mybatt.MaxStoredPower;
-                Op = mybatt.CurrentOutput;
-
-                SumSp = SumSp + Sp;
-                SumMsp = SumMsp + Msp;
-                Sumop = Sumop + Op;  
-            }
-            // Take averages of sums
-            float AvgSp = SumSp / mybatts.Count;   // Stored Power in MWh
-            float AvgMsp = SumMsp / mybatts.Count; // Max stored power in MWh
-            float AvgOp = Sumop / mybatts.Count;   // Output in MW
-
-
-
-            int pb = (int)(AvgSp / AvgMsp * 10); // Power in Seconds
-            Echo(pb.ToString());
-
-
-
-            int pn = (int)(AvgSp / AvgOp * 60 * 60);
-            string seconds = TimeSpan.FromSeconds(pn).ToString(@"hh\:mm\:ss") + "remaining";
-            Echo(seconds);
-
-        }
     }
 
 }
